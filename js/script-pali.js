c57a13b7d28eede50ec4d0c44f4a5c5985cd9d78
Alex100688 <alessiozingaro2@gmail.com>
2022-10-28 18:32:30 +0200

    Aggiunta la funzione al mio foglio di js

---
+++ js/script-pali.js
@@ -0,0 +1,24 @@
+"use strict";
+
+let parola = "esse , osso";
+let parolaDivisa=parola.split("");
+parolaDivisa=parolaDivisa.reverse();
+let parolaNuova=parolaDivisa.join("");
+console.log(parolaNuova);
+
+const ask=prompt("Inserisci la parola");
+
+
+
+function arrayListReverse(parolaNuova, ask){
+  for(let i=parolaNuova.length -1; i >= 0; i--) {
+    if ([i] === ask) {
+        return true;
+        
+    }        
+  }
+    return false;
+} 
+
+
+console.log(arrayListReverse);
